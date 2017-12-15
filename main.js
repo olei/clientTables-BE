55b8a4b7b6e1ef2e9de8ab4ecae8cb10d968e5b6
olei <wzayyy2005@126.com>
2017-12-15 17:23:01 +0800

    no message

--- main.js
+++ main.js
@@ -3,12 +3,7 @@ import history from 'connect-history-api-fallback'
 
 let app = express()
 
-// app.get('/', function(req, res){
-//   res.send('hello world')
-// })
-
 app.use(express.static('./public'))
-console.log(process.env.NODE_ENV)
 
 app.use(history())
-app.listen(3000)
+app.listen(8088)
